f716e489023726bfca3239e4c3019aa0a19fc628
Ku Syahid <kmsyahid133@gmail.com>
2024-10-06 12:25:41 +0800

    This one added test. Prev is just first repo commit

--- test.lua
+++ test.lua
@@ -1,15 +1,22 @@
 local Matrix = require("matrix")
 
 ------------------
+---Variable
+------------------
+
+local prefix = ''
+local seperator = '|'
+
+------------------
 ---TESTING
 ------------------
-print("Benchmark create matrix 20 x 20")
+print("Benchmark create matrix 4 x 4")
 local time = os.clock()
 local a = Matrix.new(4, 4)
 time = os.clock() - time
 print("Time taken: "..(time).."s")
 
-print("Benchmark create matrix 20 x 20")
+print("Benchmark create matrix 4 x 4")
 time = os.clock()
 local b = Matrix.new(4, 4)
 time = os.clock() - time
@@ -24,20 +31,37 @@ for x, y, element in b:loop() do
     b:set(x, y, math.random(-1, 1))
 end
 
-print(a:format("|"))
+print(a:format(seperator, prefix))
 print(a.y.."x"..a.x)
 print("------------------")
-print(b:format("|"))
+print(b:format(seperator, prefix))
 print(b.y.."x"..b.x)
 
-local c = (a*b)
-print("\nA * B = ")
-print(c:format('|'))
-print(c.y.."x"..c.x)
 
-print("Bench mark test start A x B. A: 4 x 4. B: 4 x 4")
+--Operation benchmark
+
+print("Bench mark test start..\nA: 4 x 4. B: 4 x 4")
+
+print("\nMultiplication")
 time = os.clock()
 local c = a*b
 time = os.clock() - time
+
+print(c:format(seperator,prefix))
+
+print("\nAddition")
+time = os.clock()
+c = a+b
+time = os.clock() - time
+
+print(c:format(seperator,prefix))
+
+print("\nSubtraction")
+time = os.clock()
+local c = a-b
+time = os.clock() - time
+
+print(c:format(seperator,prefix))
+
 print("Benchmark ends. Final time: ")
 print(time.."seconds")
